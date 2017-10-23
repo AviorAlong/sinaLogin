f519a29a5272a888ddc1c029e132c662a980eee6
AviorAlong <s2799021760@163.com>
2017-10-23 15:41:28 +0800

    加注释

--- main/index.js
+++ main/index.js
@@ -2,16 +2,15 @@ const fs = require('fs');
 const queryString = require('querystring');
 const superagent = require('superagent');
 const cheerio = require('cheerio');
-// const RSA = require('node-rsa');
 const sinaSSOEncoder = require('../sinaLib/sina.js');
-
+//url配置
 const HOME_RUL = 'http://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.19)&_=';
 const preLoginUrl = 'http://login.sina.com.cn/sso/prelogin.php?';
 const regMatchJson = /\{.*?\}/;
 const userAgent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'};
 let userInfo = {
   name: 's2799021760@163.com',
-  pwd: '080719203x'
+  // pwd: '080719203x'
 };
 let parameters = {
   'entry': 'weibo',
@@ -67,45 +66,31 @@ let getServerTime = function () {
     })
   })
 }
-
+//密码加密
 let getPwd = function (pwd, servertime, nonce, pubkey) {
-  //建一个rsa公钥，公钥的两个参数新浪微博都给了是固定值，不过给的都是16进制的字符串，
-  //一个是登录第一步中的pubkey，第二个是js加密文件中的‘10001’。
-  //这两个值需要先从16进制转换成10进制，不过也可以写死在代码里。我就把‘10001’直接写死为65537
-
-  // console.log({pwd:pwd,servertime:servertime,nonce:nonce,pubkey:pubkey})
-  // let key = new RSA({b: 1024}, 'pkcs1') //创建公钥
-  //
-  // let message = '' + servertime + '\t' + nonce + '\n' + pwd; //拼接明文 js加密文件中得到;
-  //
-  // let passwd = key.encrypt(message);//加密
-  //
-  // passwd = passwd.toString('hex');
   let RSAKey = new sinaSSOEncoder.RSAKey();
   RSAKey.setPublic(pubkey, '10001');
   let password = RSAKey.encrypt([servertime, nonce].join("\t") + "\n" + pwd);
-
-
   return password;
 }
-
+//用户名加密
 let getUser = function (name) {
   return toBase64String(name);
 }
-
+//获取登陆url
 let getLoginUrl = function (options) {
 
-  return new Promise(function (resolve,reject) {
+  return new Promise(function (resolve, reject) {
     let preJson = JSON.parse(options);
-
+    //预登陆返回的参数
     postData.servertime = preJson.servertime;
     postData.nonce = preJson.nonce;
     postData.rsakv = preJson.rsakv;
 
     postData.su = getUser(userInfo.name);
     postData.sp = getPwd(userInfo.pwd, preJson.servertime, preJson.nonce, preJson.pubkey);
-    console.log(HOME_RUL + preJson.servertime)
 
+    //登陆请求
     superagent.post(HOME_RUL + preJson.servertime).set({
       "Accept": '*/*'
     }).set({
@@ -113,63 +98,61 @@ let getLoginUrl = function (options) {
     }).set(userAgent).send(postData).end(function (err, res) {
 
       if (err) {
-        console.log('error:'+err)
-        return  reject({status:-12,message:'获取loginUrl出错'})
+        console.log('error:' + err)
+        return reject({status: -12, message: '获取loginUrl出错'})
       }
 
       let html = res.text;
+
       let urlReg = /location\.replace\(\'(.*?)\'\)/;
       let httpReg = /'http(.*)'/
-      let reUrl = html.match(urlReg)[0].match(httpReg)[0].replace(/'/g,'');
-      console.log(res.text)
-      return resolve({url:reUrl,cookie:res.headers['set-cookie']});
+      let reUrl = html.match(urlReg)[0].match(httpReg)[0].replace(/'/g, '');
+      //返回登陆url 和 cookie
+      return resolve({url: reUrl, cookie: res.headers['set-cookie']});
     })
   })
 
 }
 
 
-
+/*
+ * 获取cookie*/
 let getCookie = function (options) {
-  console.log(options)
+
   let url = options.url;
   let cookie = options.cookie;
-  return new Promise(function (resolve,reject) {
+  return new Promise(function (resolve, reject) {
     superagent.get(url).set({
       "Accept": '*/*'
     }).set({
       "Content-Type": "application/x-www-form-urlencoded",
-      'Cookie':cookie
-    }).set(userAgent).set({"Host":"passport.weibo.com","Referer":"http://weibo.com/"}).end(function (err,res) {
-      if(err){
-        return reject({status:-10,message:'获取cookie出错'});
+      'Cookie': cookie
+    }).set(userAgent).set({"Host": "passport.weibo.com", "Referer": "http://weibo.com/"}).end(function (err, res) {
+      if (err) {
+        return reject({status: -10, message: '获取cookie出错'});
       }
-      let cookie = res.header ;
-     //console.log(res.text);
-    //  console.log('==============\n\n\n',res);
-      // console.log(res.status);
-      // console.log(res['cookie']);
+      let cookie = res.header;
       return resolve(cookie);
     })
   })
-} 
-
+}
+//转base64
 function toBase64String(data) {
   let code = new Buffer(data);
   return code.toString('base64');
 }
 
-
+//登陆
 function login() {
   getServerTime().then(function (result) {
     return getLoginUrl(result);
   }).then(function (url) {
     return getCookie(url);
   }).then(function (cookie) {
-     return console.log(cookie)
+    return console.log(cookie)
 
   }).catch(function (error) {
     console.log(error.message | error)
   })
 }
- login();
+login();
